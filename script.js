2bd37bb7e3066f01cde417f1eae3ae0c39b4d6be
Will <wfoster12@gmail.com>
2018-06-16 18:47:40 +0100

    moved javascript to own file

---
+++ script.js
@@ -0,0 +1,27 @@
+var currentQ = "";
+var currentA = "";
+
+function newQuote() {
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?",
+        function (val) {
+            currentQ = val.quoteText;
+            if (val.quoteAuthor == "") {
+                val.quoteAuthor = "Anon";
+            }
+            currentA = val.quoteAuthor;
+            $('#text').html(JSON.stringify(val.quoteText));
+            $('#author').html(JSON.stringify(val.quoteAuthor));
+        });
+}
+
+$("#quote-box").on("click", function () {
+    newQuote();
+});
+
+$(document).ready(function () {
+    newQuote();
+});
+
+$("#tweet-quote").on("click", function () {
+    window.open("https://twitter.com/intent/tweet?text= %22" + currentQ + "%22 %20%20-" + currentA);
+});
